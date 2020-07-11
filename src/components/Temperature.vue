65897b84495879a36270ba31e626c2f379abb74a
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-11 15:44:45 -0300

    feat: add temperature widget

---
+++ src/components/Temperature.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+
+  .temperature-widget
+    span.current 17
+    .unit: span ºC
+    .limits
+      span.max 20º
+      span.min 17º
+
+</template>
+
+<style lang="stylus" scoped>
+
+.temperature-widget
+  font-weight 300
+
+  display grid
+  grid "current unit"\
+       "current limits"
+
+  .current
+    grid-area current
+
+    font-size 15rem
+    line-height 1
+    text-align right
+
+  .unit
+    grid-area unit
+
+    font-size 3rem
+    display flex
+    align-items flex-end
+    justify-content flex-end
+
+  .limits
+    grid-area limits
+
+    text-align right
+    display flex
+    flex-direction column
+
+</style>
