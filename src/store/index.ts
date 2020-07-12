eb7b869b591735c60dfc19a7ba6218f29ce9fca1
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-12 07:20:16 -0300

    feat: add weather store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import weatherService from '@/services/weatherService'
+import { WeatherState } from '@/types/Weather'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-  },
+  } as WeatherState,
   mutations: {
+    setWeather: (state, payload) => {
+      state.currentWeather = payload
+    }
   },
   actions: {
+    fetchWeather: async (context, { cityName }: { cityName: string }) => {
+      try {
+        // TODO: Remove this code - only to development
+        const localWheter = JSON.parse(localStorage.getItem('weather')as string)
+        if (!localWheter) {
+          throw localWheter
+        }
+        context.commit('setWeather', localWheter)
+      } catch {
+        try {
+          const { data } = await weatherService.findByCity(cityName)
+          localStorage.setItem('weather', JSON.stringify(data))
+          context.commit('setWeather', data)
+        } catch (err) {
+          console.log('err', err)
+        }
+      }
+    }
   },
-  modules: {
+  getters: {
+    temperature: (state) => {
+      const { temp, temp_max: tempMax, temp_min: tempMin } = state.currentWeather.main
+      return { temp, tempMax, tempMin }
+    }
   }
 })
