a8e6b8907bdf9c73d796c351c041369082a457bc
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-13 00:48:54 -0300

    feat: add method to load extend weather info

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -5,6 +5,17 @@ const findByCity = (cityName: string) => {
   return api.get<WeatherInfo>('/weather', { params: { q: cityName } })
 }
 
+const findHourly = (lat: number, lon: number) => {
+  return api.get('/onecall', {
+    params: {
+      lat,
+      lon,
+      exclude: 'daily,current',
+    },
+  })
+}
+
 export default {
-  findByCity
+  findByCity,
+  findHourly,
 }
