83f11ae40e5dcef8ab0af657fa255491bfa02938
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-13 00:47:28 -0300

    feat: add weather icon

---
+++ src/components/ui/WeatherIcon.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  img(:src="icon")
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class WeatherIcon extends Vue {
+  @Prop({ default: '02d' }) iconId: string
+  @Prop({ default: '' }) iconSize: string
+
+  get icon () {
+    const id = this.iconId
+    const size = this.iconSize
+    const url = `http://openweathermap.org/img/wn/${id}${size}.png`
+    return url
+  }
+}
+</script>
