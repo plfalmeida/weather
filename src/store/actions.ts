442010283f2cd8ee4d880b0fa59fb89701a67612
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-14 15:06:59 -0300

    feat: remove development code

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -6,47 +6,25 @@ const actions: ActionTree<WeatherState, WeatherState> = {
 
   fetchWeather: async (context, { cityName }: { cityName: string }) => {
     try {
-      // TODO: Remove this code - only to development
-      const localWeather = JSON.parse(localStorage.getItem('weather')as string)
-      if (!localWeather) {
-        throw localWeather
-      }
-      context.commit('setWeather', localWeather)
+      const { data } = await weatherService.findByCity(cityName)
+      localStorage.setItem('weather', JSON.stringify(data))
+      context.commit('setWeather', data)
       context.dispatch('fetchHourly', {
-        lat: localWeather.coord.lat,
-        lon: localWeather.coord.lon,
+        lat: data.coord.lat,
+        lon: data.coord.lon,
       })
-    } catch {
-      try {
-        const { data } = await weatherService.findByCity(cityName)
-        localStorage.setItem('weather', JSON.stringify(data))
-        context.commit('setWeather', data)
-        context.dispatch('fetchHourly', {
-          lat: data.coord.lat,
-          lon: data.coord.lon,
-        })
-      } catch (err) {
-        console.log('err', err)
-      }
+    } catch (err) {
+      console.log('err', err)
     }
   },
 
   fetchHourly: async (context, { lat, lon }) => {
     try {
-      // TODO: Remove this code - only to development
-      const localWeather = JSON.parse(localStorage.getItem('hourly')as string)
-      if (!localWeather) {
-        throw localWeather
-      }
-      context.commit('setHourly', localWeather)
-    } catch {
-      try {
-        const { data } = await weatherService.findHourly(lat, lon)
-        localStorage.setItem('hourly', JSON.stringify(data))
-        context.commit('setHourly', data)
-      } catch (err) {
-        console.log('err', err)
-      }
+      const { data } = await weatherService.findHourly(lat, lon)
+      localStorage.setItem('hourly', JSON.stringify(data))
+      context.commit('setHourly', data)
+    } catch (err) {
+      console.log('err', err)
     }
   },
 
