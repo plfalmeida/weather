c836cc3b49e5997f8a60fa57710d3a46079bcd62
Pedro Almeida <pedro--luiz@outlook.com>
2020-07-14 14:33:17 -0300

    feat: create custom icon component

---
+++ src/components/ui/CustomWeatherIcon.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  component(:is="ImgUrl" :data-icon-size="iconSize")
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+const images = require.context('../../assets/icons/weather/lines/', false, /\.svg$/)
+
+@Component({})
+export default class CustomWeatherIcon extends Vue {
+  @Prop({ default: '02d' }) iconId: string
+  @Prop({ default: '' }) iconSize: string
+
+  get ImgUrl () {
+    const { iconId } = this
+    const icon = images(`./${iconId}.svg`)
+    return icon
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+svg
+  width 5rem
+  fill var(--w-foreground)
+
+  &[data-icon-size="@4x"]
+    width 20rem
+
+</style>
--- src/components/ui/WeatherIcon.vue
+++ src/components/ui/WeatherIcon.vue
@@ -1,14 +1,22 @@
 <template lang="pug">
-  img(:src="icon")
+  img(v-if="!custom" :src="icon")
+  custom-weather-icon(v-else :iconId="iconId" :iconSize="iconSize")
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import CustomWeatherIcon from './CustomWeatherIcon.vue'
 
-@Component({})
+@Component({
+  name: 'weather-icon',
+  components: {
+    CustomWeatherIcon,
+  },
+})
 export default class WeatherIcon extends Vue {
   @Prop({ default: '02d' }) iconId: string
   @Prop({ default: '' }) iconSize: string
+  @Prop({ default: false }) custom: boolean
 
   get icon () {
     const id = this.iconId
